fe2da787036aaa8902fb1d17be8d43bf3ebecaa5
Dionesio Marino <dionesiomarinojr@gmail.com>
2021-02-25 22:26:52 -0700

    Edit main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String args[]) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    Scanner scanner = new Scanner(System.in);
     Calculator calculator = new Calculator();
-    String operation = reader.readLine();
+    String operation = "";
     while (true) {
       if (operation.equals("add")) {
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.add(a, b));
 
       } else if (operation.equals("subtract")) {
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.subtract(a, b));
 
       } else if (operation.equals("multiply")) {
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.multiply(a, b));
 
       } else if (operation.equals("divide")) {
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.divide(a, b));
 
       } else if (operation.equals("fibonacci")) {
-        int n = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.fibonacciNumberFinder(n));
 
       } else if (operation.equals("binary")) {
-        int n = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(scanner.nextLine());
         System.out.println(calculator.intToBinaryNumber(n));
 
       } else if (operation.equals("quit")){
         break;
       }
-      operation = reader.readLine();
+      operation = scanner.nextLine();
     }
   }
 }
